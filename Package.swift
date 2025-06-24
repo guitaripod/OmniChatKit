72e5783bcb2b9dbfe3322c9ae17410a26a528582
Marcus Ziadé <guitaripod@icloud.com>
2025-06-24 23:42:14 +0300

    Initial package setup with dependencies and platforms

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "OmniChatKit",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(
+            name: "OmniChatKit",
+            targets: ["OmniChatKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "3.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client", from: "1.19.0"),
+    ],
+    targets: [
+        .target(
+            name: "OmniChatKit",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+            ],
+            exclude: ["openapi.json", "openapi-generator-config.yaml"],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+                .enableUpcomingFeature("BareSlashRegexLiterals"),
+                .enableUpcomingFeature("ConciseMagicFile"),
+                .enableUpcomingFeature("ExistentialAny"),
+                .enableUpcomingFeature("ForwardTrailingClosures"),
+                .enableUpcomingFeature("ImplicitOpenExistentials"),
+                .enableUpcomingFeature("StrictConcurrency"),
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ]
+        ),
+        .testTarget(
+            name: "OmniChatKitTests",
+            dependencies: ["OmniChatKit"],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]
+        ),
+    ]
+)
